93a2e28833c44119c76a5b81eee726deb8040136
Marwan ROG <marwan@gitlab.ms>
2023-02-09 08:21:17 +0300

    reset

--- src/App.java
+++ src/App.java
@@ -3,27 +3,23 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) throws Exception {
     Scanner reader = new Scanner(System.in);
-    System.out.print("Enter total number of entries: ");
+    System.out.print("Enter total number of students: ");
     int count = Integer.parseInt(reader.nextLine());
     int i = 0;
-    int positive = 0;
-    int negative = 0;
-    int x = 0;
+    int top_student = -1;
+    int score = 0;
 
     while (i < count) {
-      System.out.print("Enter an integer: ");
-      x = Integer.parseInt(reader.nextLine());
+      System.out.print("Enter student score: ");
+      score = Integer.parseInt(reader.nextLine());
 
-      if (x > 0) {
-        positive++;
-      } else {
-        negative++;
+      if (score > top_student) {
+        top_student = score;
       }
       i++;
     }
 
-    System.out.println("Total positive " + positive);
-    System.out.println("Total negative " + negative);
+    System.out.println("Highest score is " + top_student);
 
     reader.close();
   }
